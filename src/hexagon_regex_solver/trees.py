7b34339a104969c35fdda22cd6a1137277aeb8f6
Derin Eryilmaz <derineryilmaz0@gmail.com>
2026-08-15 17:13:47 -0400

    oops

--- src/hexagon_regex_solver/trees.py
+++ src/hexagon_regex_solver/trees.py
@@ -37,7 +37,7 @@ def get_simpler_trees(regex_tree, limit, captures):
 
         case c.BRANCH:
             for option in value[1]:
-                yield from get_simpler_trees(option, limit, captures.copy()):
+                yield from get_simpler_trees(option, limit, captures.copy())
 
         case c.GROUPREF:
             if simple_length(captures[value], captures) <= limit:
